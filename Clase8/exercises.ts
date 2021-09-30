aa9087e5b300aa30ebbe69f2b7973f7f6227143f
Miguel Islas Hernández <43648421+MiguelIslasH@users.noreply.github.com>
2021-09-30 15:39:48 -0500

    Create exercises.ts

---
+++ Clase8/exercises.ts
@@ -0,0 +1,16 @@
+//Crea UNA función que te permita obtener la longitud de una cadena de caracteres o
+//de algún arreglo que se le pase, usando Generics
+
+//Crea UNA función que reciba un dato de tipo boolean o string o number y
+//dependiendo del tipo imprima en consola "booleano" "cadena de caracteres" o "número"
+//Respectivamente, usando generics;
+
+//Crear una clase Contenedor, que te permita: guardar, leer e imprimir todos sus items
+//esta clase debe trabajar con un arreglo de items, estos items pueden ser de las siguientes
+//interfaces: Juguete, Ropa, Botella o Libro
+//Guardar: Se le pasa como parametro un objeto del tipo Juguete, Ropa,Botella o Libro y lo guarda en tu lista de items
+//Leer: Se le pasa como parámetro el índice del item a regresar y lo retorna
+//imprimir: No recibe parámetros e imprime todos tus items
+//La clase debe implementar generics class Contenedor<T>{}
+//para instanciarla: new Contenedor<Juguete | Ropa | Botella | Libro>();
+//y probarla
